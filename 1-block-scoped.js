f6ae709556d506fe049bf9f0bc364b1b78e1ff0d
keziahNsoh <porepeya@gmail.com>
2024-10-01 00:52:31 +0000

    remove nod modules dir, fix reassignment

--- 1-block-scoped.js
+++ 1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;  // Change var to let
-  let task2 = true;  // Change var to let
+  const task = false;  // Change var to let
+  const task2 = true;  // Change var to let
 
   if (trueOrFalse) {
-    task = true;      // No need to redeclare; just assign
-    task2 = false;    // No need to redeclare; just assign
+    const task3 = true;      // No need to redeclare; just assign
+    const task4 = false;    // No need to redeclare; just assign
   }
 
   return [task, task2];
